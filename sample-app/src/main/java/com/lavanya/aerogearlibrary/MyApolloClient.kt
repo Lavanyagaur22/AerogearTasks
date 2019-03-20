facdc5dbefd60d9078589164a8ea47ec06e034e3
Lavanya Gaur <lavanyagaur22@gmail.com>
2019-03-20 21:23:09 +0530

    configured backend and apollo and corrected .gitignore folder

---
+++ sample-app/src/main/java/com/lavanya/aerogearlibrary/MyApolloClient.kt
@@ -0,0 +1,31 @@
+package com.lavanya.aerogearlibrary
+
+import com.apollographql.apollo.ApolloClient
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+class MyApolloClient {
+
+    companion object {
+
+        private val BASE_URL = "http://192.168.0.105:4000/graphql"
+
+        private var myApolloClient: ApolloClient? = null
+
+        fun getMyApolloClient(): ApolloClient? {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+
+            myApolloClient = ApolloClient.builder()
+                .serverUrl(BASE_URL)
+                .okHttpClient(okHttpClient)
+                .build()
+
+            return myApolloClient
+        }
+    }
+}
